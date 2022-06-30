d55e97813b3544b900d6d308582a0f277c637bf4
Ajdin Imsirovic <ajdika@live.com>
2022-06-30 15:15:34 +0200

    (2) Remember passing data as props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="cars">
-      <h1>My favorite cars</h1>
-      <Car make="BMW" color="blue" year="2020" />
-    </div>
+    <Car
+        make="BMW"
+        color="blue"
+        year="2020"
+        carImgSrc="https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg"
+    />
   );
 }
 
--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,7 @@
 function Car(props) {
-
-    const carImgSrc = "https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg"
-
     return (
       <div>
-        <img src={carImgSrc} alt="My favorite car" />
+        <img src={props.carImgSrc} alt="My favorite car" />
         <p>One of my favorite cars is a {props.color} {props.make} from {props.year}.</p>
       </div>
     )
