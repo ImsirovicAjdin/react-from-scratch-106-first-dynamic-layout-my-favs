e930e6d83e9c9f715ad2db2527dc6baed0676413
Ajdin Imsirovic <ajdika@live.com>
2022-06-30 15:16:43 +0200

    (3) Invoking multiple instances of the same child component inside a parent component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <Car
+    <div>
+      <h1>My favorite cars</h1>
+      <Car
         make="BMW"
         color="blue"
         year="2020"
-        carImgSrc="https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg"
-    />
+        carImgSrc="https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg" />
+      <Car
+        make="Mercedes"
+        color="gray"
+        year="2018"
+        carImgSrc="https://www.codingexercises.com/img/2022-01-25/002-mercedes-gray.jpg" />
+      <Car
+        make="Toyota"
+        color="gray"
+        year="new"
+        carImgSrc="https://www.codingexercises.com/img/2022-01-25/003-toyota-gray.jpg" />
+      <Car
+        make="Alfa Romeo"
+        color="red"
+        year="2012"
+        carImgSrc="https://www.codingexercises.com/img/2022-01-25/004-alpha-romeo-red.jpg" />
+    </div>
   );
 }
 
